1b6a024da4bb6f5ea15e4c2ab27941c1e228e131
QiaN HuNTeR <qianqian1903@gmail.com>
2022-03-10 00:30:47 +0800

    complete version

---
+++ app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form");
+const email = document.querySelector("input[type=text]");
+const errorMsg = document.querySelector(".form__error-msg");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	// when button click, remove error msg and email input border first before checking
+	errorMsg.textContent = "";
+	email.classList.remove("danger");
+
+	console.log("asdf");
+
+	if (!email.value.trim()) {
+		errorMsg.textContent = "Oops! Please add your email";
+		email.classList.add("danger");
+		return;
+	}
+
+	if (!email.value.includes("@")) {
+		errorMsg.textContent = "Oops! That doesn’t look like an email address";
+		email.classList.add("danger");
+		return;
+	}
+
+	alert("We'll notify you once we launch!");
+	errorMsg.textContent = "";
+	email.value = "";
+});
